e6ea39e3c8f5753335174f9ba744053c35507157
LogeshD2 <106709343+LogeshD2@users.noreply.github.com>
2024-11-26 09:52:33 +0100

    Revert "Exercice 3"

--- projet_shellcode.s
+++ projet_shellcode.s
@@ -1,8 +1,5 @@
 section .data
-fichier db "dossier", 0
-
-msg_error_open_file db "Erreur lors de l'ouverture du fichier", 10, 0
-len_msg_error_open_file equ $-msg_error_open_file
+fichier db "ls_copy", 0
 
 msg_not_elf db "Ce n'est pas un fichier elf", 10, 0
 len_msg_not_elf equ $-msg_not_elf
@@ -10,12 +7,9 @@ len_msg_not_elf equ $-msg_not_elf
 msg_is_elf db "C'est un fichier elf", 10, 0
 len_msg_is_elf equ $-msg_is_elf
 
-msg_is_folder db "C'est un dossier", 10, 0
-len_msg_is_folder equ $-msg_is_folder
 
 section .bss
 fd resq 1
-statbuf resb 144
 buffer resb 256
 
 section .text
@@ -29,25 +23,6 @@ _start:
 	xor rdx, rdx
 	syscall
 	mov [fd], rax
-
-	; Vérifie erreur ouverture
-	cmp rax, 0
-	js error_open_file
-	
-
-	; Vérifie si c'est un dossier
-        mov rax, 4
-        lea rdi, [fichier]
-        lea rsi, [statbuf]
-	syscall
-
-
-	; Vérifie le champ st_mode
-	mov eax, dword [statbuf + 24]
-	and eax, 0xF000
-	cmp eax, 0x4000
-	je is_folder	
-
 	
 	; Lecture fichier
 	mov rax, 0
@@ -119,21 +94,4 @@ is_elf:
 	mov rdx, len_msg_is_elf
 	syscall
 	jmp exit
-
-is_folder:
-	mov rax, 1
-	mov rdi, 1
-	lea rsi, [msg_is_folder]
-	mov rdx, len_msg_is_folder
-	syscall
-	jmp exit
-
-error_open_file:
-	mov rax, 1
-	mov rdi, 1
-	lea rsi, [msg_error_open_file]
-	mov rdx, len_msg_error_open_file
-	syscall
-	jmp exit
-
 
